65d62c4330eb3451bd9fda804eef5634de8fbe11
Sam Hatcliff <music@Sam-Hatcliffs-Macbook-Pro.local>
2021-10-04 15:36:01 -0600

    adding new files from units 13 and 14

--- unit_14/axios.js
+++ unit_14/axios.js
@@ -1,4 +1,47 @@
+// const response = axios.get('https://swapi.dev/api/planets')
+// async function getData() {
+//     const response = await axios.get('https://swapi.dev/api/planets');
+//     const { next, results } = response.data;
+//     for (let planet of results) {
+//         console.log(planet.name)
+//     }
+//     const res2 = await axios.get(next);
+//     console.log(res2.data.results[0].name);
 
+// }
+// getData();
+// console.log("ijoihigu");
+
+// async function getLaunches() {
+//     const res = await axios.get('https://api.spacexdata.com/v3/launches/upcoming');
+//     renderLaunches(res.data);
+// }
+
+
+// function renderLaunches(launches) {
+//     const ul = document.querySelector('#launches');
+//     for (let launch of launches) {
+//         const newLI = document.createElement('LI');
+//         const mission = document.createElement('B')
+//         mission.innerText = launch.mission_name;
+//         newLI.append(mission);
+//         newLI.innerHTML += ` - ${launch.rocket.rocket_name}`
+//         ul.append(newLI);
+//     }
+// }
+
+// const btn = document.querySelector('#getLaunches');
+// btn.addEventListener('click', getLaunches)
+
+// async function getUsers() {
+//     const res = await axios.get('https://reqres.in/api/users?page=2');
+//     console.log(res);
+// }
+
+// async function createUser() {
+//     const res = await axios.post('https://reqres.in/api/users?page=2', { pooop: 'ok', test: 'yeaaa' })
+//     console.log(res)
+// }
 
 async function getUser(token) {
     const res = await axios.get('https://hack-or-snooze-v3.herokuapp.com/users/username', { params: { token } })
