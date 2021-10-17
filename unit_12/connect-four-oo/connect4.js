c22695247560873fa192ca642ec2bed7ce66be7c
Sam Hatcliff <music@Sam-Hatcliffs-Macbook-Pro.local>
2021-10-16 21:03:56 -0600

    added oop conect4

---
+++ unit_12/connect-four-oo/connect4.js
@@ -0,0 +1,158 @@
+/** Connect Four
+ *
+ * Player 1 and 2 alternate turns. On each turn, a piece is dropped down a
+ * column until a player gets four-in-a-row (horiz, vert, or diag) or until
+ * board fills (tie)
+ */
+
+class Game {
+  constructor(p1, p2, WIDTH = 7, HEIGHT = 8) {
+    this.player1 = p1
+    this.player2 = p2
+    this.WIDTH = WIDTH;
+    this.HEIGHT = HEIGHT;
+    this.currPlayer = this.player1;
+    this.makeBoard();
+    this.makeHtmlBoard();
+    this.gameOver = false;
+
+  }
+  makeBoard() {
+    this.board = []
+    for (let y = 0; y < this.HEIGHT; y++) {
+      this.board.push(Array.from({ length: this.WIDTH }));
+    }
+  }
+
+  makeHtmlBoard() {
+    const board = document.getElementById('board');
+
+    // make column tops (clickable area for adding a piece to that column)
+    const top = document.createElement('tr');
+    top.setAttribute('id', 'column-top');
+    this.handleGameClick = this.handleClick.bind(this);
+    top.addEventListener("click", this.handleGameClick);
+    //dont understand
+
+    for (let x = 0; x < this.WIDTH; x++) {
+      const headCell = document.createElement('td');
+      headCell.setAttribute('id', x);
+      top.append(headCell);
+    }
+
+    board.append(top);
+
+    // make main part of board
+    for (let y = 0; y < this.HEIGHT; y++) {
+      const row = document.createElement('tr');
+
+      for (let x = 0; x < this.WIDTH; x++) {
+        const cell = document.createElement('td');
+        cell.setAttribute('id', `${y}-${x}`);
+        row.append(cell);
+      }
+
+      board.append(row);
+    }
+  }
+  findSpotForCol(x) {
+    for (let y = this.HEIGHT - 1; y >= 0; y--) {
+      if (!this.board[y][x]) {
+        return y;
+      }
+    }
+    return null;
+  }
+  placeInTable(y, x) {
+    const piece = document.createElement('div');
+    piece.classList.add('piece');
+    piece.style.backgroundColor = this.currPlayer.color;
+    piece.style.top = -50 * (y + 2);
+
+    const spot = document.getElementById(`${y}-${x}`);
+    spot.append(piece);
+  }
+  endGame(msg) {
+    alert(msg);
+    const top = document.querySelector("#column-top");
+    top.removeEventListener("click", this.handleGameClick);
+  }
+
+
+  handleClick(evt) {
+    // get x from ID of clicked cell
+    const x = +evt.target.id;
+    // get next spot in column (if none, ignore click)
+    const y = this.findSpotForCol(x);
+    if (y === null) {
+      return;
+    }
+    // place piece in board and add to HTML table
+    this.board[y][x] = this.currPlayer;
+    this.placeInTable(y, x);
+
+    // check for tie
+    if (this.board.every(row => row.every(cell => cell))) {
+      return this.endGame('Tie!');
+    }
+
+    // check for win
+    if (this.checkForWin()) {
+      this.gameOver = true;
+      return this.endGame(`The ${this.currPlayer.color} player won!`);
+    }
+
+    if (this.currPlayer === this.player1) {
+      this.currPlayer = this.player2;
+    } else if (this.currPlayer === this.player2) {
+      this.currPlayer = this.player1;
+    }
+    console.log(this.board);
+  }
+  checkForWin() {
+    // Check four cells to see if they're all color of current player
+    //  - cells: list of four (y, x) cells
+    //  - returns true if all are legal coordinates & all match currPlayer
+    const _win = cells =>
+      cells.every(
+        ([y, x]) =>
+          y >= 0 &&
+          y < this.HEIGHT &&
+          x >= 0 &&
+          x < this.WIDTH &&
+          this.board[y][x] === this.currPlayer
+      );
+
+    for (let y = 0; y < this.HEIGHT; y++) {
+      for (let x = 0; x < this.WIDTH; x++) {
+        // get "check list" of 4 cells (starting here) for each of the different
+        // ways to win
+        const horiz = [[y, x], [y, x + 1], [y, x + 2], [y, x + 3]];
+        const vert = [[y, x], [y + 1, x], [y + 2, x], [y + 3, x]];
+        const diagDR = [[y, x], [y + 1, x + 1], [y + 2, x + 2], [y + 3, x + 3]];
+        const diagDL = [[y, x], [y + 1, x - 1], [y + 2, x - 2], [y + 3, x - 3]];
+
+        // find winner (only checking each win-possibility as needed)
+        if (_win(horiz) || _win(vert) || _win(diagDR) || _win(diagDL)) {
+          return true;
+        }
+      }
+    }
+  }
+}
+document.getElementById('start').addEventListener('click', (e) => {
+  e.preventDefault();
+  let p1color = document.getElementById("p1color").value || "red";
+  let p2color = document.getElementById("p2color").value || "blue";
+  let p2 = new Player(p2color, 2);
+  let p1 = new Player(p1color, 1);
+  let width = document.getElementById('width').value || 7
+  let height = document.getElementById('height').value || 8
+  const currGame = new Game(p1, p2, width, height);
+})
+class Player {
+  constructor(color, number) {
+    this.color = color;
+    this.number = number;
+  }
+}
---
+++ unit_18/classexercise.py
@@ -0,0 +1,10 @@
+class SerialGenerator:
+    "every time you ask for a new number, it should return the next sequential number:"
+
+    def __init__(self, initial):
+        self.initial = initial
+
+    def generate(self):
+
+    count = self.initial
+    def reset(self)
